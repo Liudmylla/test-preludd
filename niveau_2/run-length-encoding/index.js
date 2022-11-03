4a2069183b435ef1afb6065768143ab98b0779ab
Liudmylla <liudmyla12@yahoo.fr>
2022-11-03 15:58:48 +0100

    encoding bug fixed

--- niveau_2/run-length-encoding/index.js
+++ niveau_2/run-length-encoding/index.js
@@ -4,9 +4,9 @@ const runLengthEncoding = (str) => {
     previous,
     count;
 
-  for (count = 1, previous = arr[0], i = 1; i < arr.length; i++) {
+  for (count = 1, previous = arr[0], i = 1; i < arr.length + 1; i++) {
     if (arr[i] !== previous) {
-      count === 1 ? encoding.push(previous) : encoding.push(count, previous);
+      encoding.push(count, previous);
       count = 1;
       previous = arr[i];
     } else {
@@ -14,31 +14,40 @@ const runLengthEncoding = (str) => {
     }
   }
 
-  encoding.push(count, previous);
+  encoding.push(count);
+  const result = encoding.slice(0, encoding.length - 1);
 
-  return console.log(encoding.join(""));
+  return console.log(`${str}` + " => " + result.join(""));
 };
+console.log("Encoding: ");
 runLengthEncoding("AABCCCDEEEE");
-
+runLengthEncoding("AABCCCDE");
+runLengthEncoding("ABCCCDDE");
+console.log("Decoding: ");
 const runLengthDecoding = (str) => {
   let arr = [...str];
   let modifiedArr = [];
-  arr.map((item, index) => {
-    if (index % 2 === 0) {
+  arr.map((item) => {
+    if (!isNaN(parseInt(item))) {
       item = Number(item);
-    }
+    } else item = item;
     modifiedArr.push(item);
   });
+
   let decoding = [];
   modifiedArr.map((item, index) => {
-    if (index % 2 === 0) {
-      decoding.push(...Array(item).fill(arr[index + 1]));
-    }
+    decoding.push(...Array(item).fill(arr[index + 1]));
   });
 
-  return console.log(decoding.join(""));
+  const result = [];
+  decoding.map((item) => {
+    if (!isNaN(parseInt(item))) {
+      item = null;
+    } else item = item;
+    result.push(item);
+  });
+
+  return console.log(`${str}` + " => " + result.join(""));
 };
-runLengthDecoding("2A1B3C1D4E");
-//todo transform input data from 2AB3CD4E to 2A1B3C1D4E :
-// check each item and prev item in array
-// if Number(item) isNan && Number( prev item) isNan insert 1
+runLengthDecoding("2AB3CD4E");
+runLengthDecoding("3AB6C2DE");
